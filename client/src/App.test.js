aa21a49637f30b1621ef3ed9b77161bd5c6d5231
Nicole Hollis <nicole@emmabeauty.co>
2019-10-07 16:00:13 -0700

    Added test for data render

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import * as rtl from "@testing-library/react"
 import ReactDOM from 'react-dom';
 import App from './App';
+import PlayerCard from "./components/PlayerCard";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -10,6 +11,11 @@ it('renders without crashing', () => {
 });
 
 it('renders "soccer searches" text', () => {
-  const wrapper = rtl.render(<App />);
-  expect(wrapper.getByText(/soccer searches/i));
+  const h1 = rtl.render(<App />);
+  expect(h1.getByText(/soccer searches/i));
+});
+
+it('renders player data', () => {
+  const div = rtl.render(<PlayerCard />);
+  expect(div.getByTestId("playerData"));
 });
--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -5,7 +5,7 @@ class PlayerCard extends Component {
 
   render() {
     return (
-      <div key={this.props.id}>
+      <div data-testid="playerData" key={this.props.id}>
         <h2>{`Name: ${this.props.name}`}</h2>
         <h2>{`Country: ${this.props.country}`}</h2>
         <h2>{`Searches: ${this.props.searches}`}</h2>
